9979673e2d37fd24c649d7c5b8c0e0c0400706d1
GabBloc <gknyagblordzro@st.ug.edu.gh>
2024-06-30 02:43:32 +0000

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -8,3 +8,15 @@ function processArray(arr) {
     });
 }
 module.exports = { processArray };
+
+
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase(); 
+        } else {
+            return str.toLowerCase(); 
+        }
+    });
+}
+module.exports = { processArray, formatArrayStrings };
